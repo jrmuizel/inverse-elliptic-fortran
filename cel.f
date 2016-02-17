56525f5de61f4f21268ffddbcc694adadfe720a7
Jeff Muizelaar <jmuizelaar@mozilla.com>
2016-02-17 10:49:34 -0500

    Reformatting to be closer to the original

--- cel.f
+++ cel.f
@@ -21,7 +21,7 @@ C
 C
       FUNCTION CEL(QQC,PP,AA,BB)
       REAL*8 CEL,QQC,PP,AA,BB,A,B,P,E,EM,QC,F,G,Q
-      PARAMETER (CA=.0003, PI02=1.5707963268)
+      PARAMETER (CA=.0003, PIO2=1.5707963268)
       IF(QQC.EQ.0.)PAUSE 'failure in CEL'
       QC=ABS(QQC)
       A=AA
@@ -30,17 +30,17 @@ C
       E=QC
       EM=1.
       IF(P.GT.0.)THEN
-              P=SQRT(P)
-              B=B/P
+        P=SQRT(P)
+        B=B/P
       ELSE
-              F=QC*QC
-              Q=1.-F
-              G=1.-P
-              F=F-P
-              Q=Q*(B-A*P)
-              P=SQRT(F/G)
-              A=(A-B)/G
-              B=-Q/(G*G*P)+A*P
+        F=QC*QC
+        Q=1.-F
+        G=1.-P
+        F=F-P
+        Q=Q*(B-A*P)
+        P=SQRT(F/G)
+        A=(A-B)/G
+        B=-Q/(G*G*P)+A*P
       ENDIF
 1     F=A
       A=A+B/P
@@ -51,11 +51,11 @@ C
       G=EM
       EM=QC+EM
       IF(ABS(G-QC).GT.G*CA)THEN
-              QC=SQRT(E)
-              QC=QC+QC
-              E=QC*EM
-              GO TO 1
+        QC=SQRT(E)
+        QC=QC+QC
+        E=QC*EM
+        GO TO 1
       ENDIF
-      CEL=PI02*(B+A*EM)/(EM*(EM+P))
+      CEL=PIO2*(B+A*EM)/(EM*(EM+P))
       RETURN
       END
